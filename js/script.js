62f5ef8bc36d38070f68a3c2a36915d425b934b5
goldwinalec <63246019+goldwinalec@users.noreply.github.com>
2021-03-26 14:31:35 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: false,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-});
+$('.opportunities__items').slick(
+    {
+        dots: true,
+        infinite: false
+    }
+);
